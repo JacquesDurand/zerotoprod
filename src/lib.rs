e2ce3b77a8c08ea2faddf6833f159250195915f9
Hugo Nicolas <hugo.nicolas.hn@gmail.com>
2022-02-06 16:57:08 +0100

    feat(email): add subscription route + tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,28 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
 use std::net::TcpListener;
+
+#[derive(serde::Deserialize)]
+struct SubscriptionForm {
+    name: String,
+    email: String,
+}
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
+async fn subscribe(form: web::Form<SubscriptionForm>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
 
 pub fn run(tcp_listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health-check", web::get().to(health_check)))
-        .listen(tcp_listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health-check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(tcp_listener)?
+    .run();
 
     Ok(server)
 }
