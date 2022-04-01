24184dcc82a4e4c498e55aa6d11d101d80b5de83
Hugo Nicolas <hugo.nicolas.hn@gmail.com>
2022-04-01 09:03:14 +0200

    chore(telemetry): wip chapter 4

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -13,6 +13,14 @@ pub async fn subscribe(
     form: web::Form<SubscriptionForm>,
     pg_pool: web::Data<PgPool>,
 ) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    tracing::info!(
+        "request_id: {} Adding {}, {} as a new subscriber",
+        request_id,
+        form.name,
+        form.email
+    );
+    tracing::info!("request_id: {} Saving new subscriber info into the database", request_id);
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions(id, email, name, subscribed_at)
@@ -26,9 +34,12 @@ pub async fn subscribe(
     .execute(pg_pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            tracing::info!("request_id: {} New subscriber successfully saved into the database", request_id);
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            tracing::error!("request_id: {} Failed to execute query: {:?}", request_id, e);
             HttpResponse::InternalServerError().finish()
         }
     }
